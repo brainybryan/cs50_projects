59fc363c5d2b1ca05bb1fdc6d81272d16ef39a16
Bryan <bryan@sach.sg>
2023-10-12 10:52:39 +0800

    commit 1

--- commerce/commerce/auctions/views.py
+++ commerce/commerce/auctions/views.py
@@ -4,11 +4,65 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Category, Listing
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    active_listings = Listing.objects.filter(is_active=True)
+    all_categories = Category.objects.all()
+    return render(request, "auctions/index.html", {
+        "listings": active_listings,
+        "categories": all_categories
+    })
+
+
+def display_category(request):
+    if request.method == "POST":
+        category_from_form = request.POST['category']
+        category = Category.objects.get(category_name=category_from_form)
+        active_listings = Listing.objects.filter(is_active=True, category=category)
+        all_categories = Category.objects.all()
+        return render(request, "auctions/index.html", {
+            "listings": active_listings,
+            "categories": all_categories
+        })
+
+
+def create_listing(request):
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        return render(request, "auctions/create.html", {
+            "categories": all_categories
+        })
+    else:
+        # Get the data from the form
+        title = request.POST["title"]
+        description = request.POST["description"]
+        image_url = request.POST["image_url"]
+        price = request.POST["price"]
+        category = request.POST["category"]
+
+        # Who is the user
+        current_user = request.user
+
+        # Get category data
+        category_data = Category.objects.get(category_name=category)
+
+        # Create a new listing object
+        new_listing = Listing(
+            title=title,
+            description=description,
+            image_url=image_url,
+            price=float(price),
+            category=category_data,
+            owner=current_user
+        )
+
+        # Insert the object in our database
+        new_listing.save()
+
+        # Redirect to index page
+        return HttpResponseRedirect(reverse(index))
 
 
 def login_view(request):
